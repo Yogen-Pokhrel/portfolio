4665c917c66935c85e9ae0668d9a9e58555438fe
Yogen <yogendrapokhrel777@gmail.com>
2024-11-30 01:00:44 -0600

    Minor update, PaginationDto is now deprecated and will be removed in next update

--- backend/src/main/java/com/portfolio/common/dto/PaginationDto.java
+++ backend/src/main/java/com/portfolio/common/dto/PaginationDto.java
@@ -2,14 +2,16 @@ package com.portfolio.common.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PaginationDto {
 
     @Min(value = 1, message = "Page number must be at least 1")
     private int page = 1;
 
-    @Positive(message = "Size must be a positive number")
+    @Min(value = 1, message = "Size must be a positive number")
     private int size = 10;
 }
