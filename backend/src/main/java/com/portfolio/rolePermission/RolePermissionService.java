00a6bc49c43d3c99d3a6435ead975f6d98318f92
Yogen <yogendrapokhrel777@gmail.com>
2024-11-30 00:56:29 -0600

    Role Permission CRUD, yet to implement user authorization

--- backend/src/main/java/com/portfolio/rolePermission/RolePermissionService.java
+++ backend/src/main/java/com/portfolio/rolePermission/RolePermissionService.java
@@ -1,22 +1,29 @@
 package com.portfolio.rolePermission;
 
+import com.portfolio.auth.AuthDetails;
+import com.portfolio.common.Permission;
 import com.portfolio.rolePermission.repository.RolePermissionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RolePermissionService {
 
     private final RolePermissionRepository rolePermissionRepository;
 
-    public boolean checkPermission(Authentication auth, Collection<String> permissions)
+    public boolean checkPermission(Authentication auth,  Collection<Permission> permissions)
     {
+        log.info("{} {}", auth.getPrincipal(), auth.getAuthorities());
+        if(!(auth.getPrincipal() instanceof AuthDetails)){
+            return false;
+        }
+        permissions.forEach((e) -> System.out.println(e.getAction() + " " + e.getDomain()));
      return true;
     }
 }
