e75434bcd38f5c1b3dfdee3c6984b2527916a65e
Yogen <yogendrapokhrel777@gmail.com>
2024-12-08 23:00:21 -0600

    Configurations moved to core

---
+++ auth/src/main/java/com/portfolio/auth/configuration/PaginationConfig.java
@@ -0,0 +1,10 @@
+package com.portfolio.auth.configuration;
+
+import com.portfolio.core.config.BasePaginationConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class PaginationConfig extends BasePaginationConfig {
+}
