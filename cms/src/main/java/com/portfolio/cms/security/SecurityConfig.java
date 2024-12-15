e144b0610fdfd0b7af70b8fd6fd2da819f783c88
Yogen <yogendrapokhrel777@gmail.com>
2024-12-14 18:15:53 -0600

    CMS service initialization

---
+++ cms/src/main/java/com/portfolio/cms/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.portfolio.cms.security;
+
+import com.portfolio.core.security.BaseSecurityConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig extends BaseSecurityConfig {
+}
