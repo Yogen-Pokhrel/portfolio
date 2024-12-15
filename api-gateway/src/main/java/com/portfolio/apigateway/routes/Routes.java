1a3c5a5bc9628c246fd506c8448b223b48087dcf
Yogen <yogendrapokhrel777@gmail.com>
2024-12-15 09:47:50 -0600

    Swagger Configuration and circuit breaker pattern imlemented

---
+++ api-gateway/src/main/java/com/portfolio/apigateway/routes/Routes.java
@@ -0,0 +1,73 @@
+package com.portfolio.apigateway.routes;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+
+@Configuration
+public class Routes {
+
+    @Value("${microservice.portfolio-account-url}")
+    private String portfolioAccountServiceUrl;
+
+    @Value("${microservice.portfolio-cms-url}")
+    private String portfolioCmsServiceUrl;
+
+    @Bean
+    public RouterFunction<ServerResponse> accountServiceRoute() {
+        return route("account_service")
+                .route(RequestPredicates.path("/account"), HandlerFunctions.http(portfolioAccountServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> accountServiceSwaggerRoute() {
+        return route("account_service_swagger")
+                .route(RequestPredicates.path("/aggregate/account-service/v3/api-docs"), HandlerFunctions.http(portfolioAccountServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> cmsServiceRoute() {
+        return route("cms_service")
+                .route(RequestPredicates.path("/cms"), HandlerFunctions.http(portfolioCmsServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> cmsServiceSwaggerRoute() {
+        return route("cms_service_swagger")
+                .route(RequestPredicates.path("/aggregate/cms-service/v3/api-docs"), HandlerFunctions.http(portfolioCmsServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
+                .build();
+    }
+}
