b52be1a158106f5d74af00af05d40cc32de94dab
Yogen <yogendrapokhrel777@gmail.com>
2024-12-18 13:17:42 -0600

    Core related fixes

--- cms/src/main/java/com/portfolio/cms/experience/ExperienceController.java
+++ cms/src/main/java/com/portfolio/cms/experience/ExperienceController.java
@@ -1,7 +1,15 @@
 package com.portfolio.cms.experience;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cms/experience")
 public class ExperienceController {
+
+    @GetMapping
+    public String experience() {
+        return "experience";
+    }
 }
