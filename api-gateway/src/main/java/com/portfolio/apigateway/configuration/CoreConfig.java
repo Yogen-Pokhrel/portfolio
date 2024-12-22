46f44788816181ca14e9626cb36658c66578d811
Yogen <yogendrapokhrel777@gmail.com>
2024-12-22 12:59:47 -0600

    cleanup

--- api-gateway/src/main/java/com/portfolio/apigateway/configuration/CoreConfig.java
+++ api-gateway/src/main/java/com/portfolio/apigateway/configuration/CoreConfig.java
@@ -1,6 +1,7 @@
 package com.portfolio.apigateway.configuration;
 
 import com.portfolio.core.config.KeycloakConfig;
+import com.portfolio.core.config.MicroserviceAddressResolver;
 import com.portfolio.core.helpers.FileUploaderService;
 import com.portfolio.core.helpers.ListMapper;
 import com.portfolio.core.helpers.LocalFileUploaderServiceImpl;
@@ -10,8 +11,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableConfigurationProperties(KeycloakConfig.class)
+@Import(MicroserviceAddressResolver.class)
 public class CoreConfig {
 }
