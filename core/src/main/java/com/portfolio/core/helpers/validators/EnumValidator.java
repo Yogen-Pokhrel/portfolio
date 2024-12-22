cad3af569e040c176303b99040cd624c0eeac671
Yogen <yogendrapokhrel777@gmail.com>
2024-12-22 14:27:06 -0600

    Enum validator with custom message and suggestions on failure, along with required attribute added to control null checks

--- core/src/main/java/com/portfolio/core/helpers/validators/EnumValidator.java
+++ core/src/main/java/com/portfolio/core/helpers/validators/EnumValidator.java
@@ -4,25 +4,49 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
 
 public class EnumValidator implements ConstraintValidator<ValidEnum, Enum<?>> {
 
     private Enum<?>[] enumValues;
+    private boolean required;
+    private String messageTemplate;
 
     @Override
     public void initialize(ValidEnum constraintAnnotation) {
         Class<? extends Enum<?>> enumClass = constraintAnnotation.enumClass();
         this.enumValues = enumClass.getEnumConstants();
+        this.required = constraintAnnotation.required();
+        this.messageTemplate = constraintAnnotation.message();
     }
 
     @Override
     public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
-        if (value == null) {
-            return true; // Allow null; use @NotNull or @NotBlank for non-null constraints
+        if (value == null && !required) {
+            createConstraintViolationMessage(context);
+            return false;
         }
 
-        // Check if the value is part of the enum
-        return Arrays.stream(enumValues)
-                .anyMatch(enumValue -> enumValue.equals(value));
+        boolean isValid = Arrays.asList(enumValues).contains(value);
+        if (!isValid) {
+            createConstraintViolationMessage(context);
+        }
+        return isValid;
+
+    }
+
+    private void createConstraintViolationMessage(ConstraintValidatorContext context) {
+        context.disableDefaultConstraintViolation();
+
+        String enumValuesString = Arrays.stream(enumValues)
+                .map(Enum::name)
+                .collect(Collectors.joining(", "));
+        String finalMessage = messageTemplate.contains("{values}")
+                ? messageTemplate.replace("{values}", enumValuesString)
+                : messageTemplate;
+
+        context.buildConstraintViolationWithTemplate(finalMessage)
+                .addConstraintViolation();
     }
 }
