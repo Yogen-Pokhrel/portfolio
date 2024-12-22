2f6f916b57b852a2484c12ff6181b60e0e8c2d7f
Yogen <yogendrapokhrel777@gmail.com>
2024-12-22 12:54:27 -0600

    log type changed to error and HttpMessageNotReadableException exception is configured to send a structured error object such that front-end can map and show errors on respective fields

--- core/src/main/java/com/portfolio/core/exception/BaseExceptionHandler.java
+++ core/src/main/java/com/portfolio/core/exception/BaseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.portfolio.core.exception;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.portfolio.core.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
@@ -15,31 +16,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.portfolio.core.util.Utils.extractSimpleClassName;
 
 @Slf4j
 public class BaseExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ApiResponse<String>> handleAuthenticationException(AuthenticationException ex) {
-        log.debug("Authentication failed: {}" ,ex.getMessage());
+        log.error("Authentication failed: {}" ,ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("Authentication failed: " + ex.getMessage()));
     }
 
     @ExceptionHandler(AuthorizationDeniedException.class)
     public ResponseEntity<ApiResponse<String>> handleAuthenticationException(AuthorizationDeniedException ex) {
-        log.debug("Authorization failed: {}" ,ex.getMessage());
+        log.error("Authorization failed: {}" ,ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiResponse.error("Forbidden resource: " + ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<String>> handleAuthenticationException(IllegalArgumentException ex) {
-        log.debug("IllegalArgumentException: {}" ,ex.getMessage());
+        log.error("IllegalArgumentException: {}" ,ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("Internal server error, please contact admin team to resolve any issues, message: " + ex.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<?>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        log.debug("Validation failed: {}" ,ex.getMessage());
+        log.error("Validation failed: {}" ,ex.getMessage());
         Map<String, List<String>> errors = new HashMap<>();
 
         ex.getBindingResult()
@@ -58,23 +63,36 @@ public class BaseExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Map<String, String>> handleEmptyBodyException(HttpMessageNotReadableException ex) {
-        log.debug("HttpMessageNotReadableException: {}" ,ex.getMessage());
-        Map<String, String> error = new HashMap<>();
-        error.put("error", "Request body is missing or invalid");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    public ResponseEntity<ApiResponse<?>> handleJsonParseException(HttpMessageNotReadableException ex) {
+        log.error("JSON Parsing error: {}", ex.getMessage());
+
+        Map<String, List<String>> errors = new HashMap<>();
+        if (ex.getCause() instanceof JsonMappingException jsonMappingException) {
+            String simplifiedMessage = extractSimpleClassName(jsonMappingException.getOriginalMessage());
+            for(JsonMappingException.Reference ref : jsonMappingException.getPath()) {
+                List<String> errorMessages = errors.getOrDefault(ref.getFieldName(), new ArrayList<>());
+                errorMessages.add(simplifiedMessage);
+                errors.put(ref.getFieldName(), errorMessages);
+            }
+        } else {
+            errors.put("general", new ArrayList<>() {{
+                add(ex.getMessage());
+            }});
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("Bad Request", errors));
     }
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ApiResponse<String>> handleDatabaseException(DataAccessException ex) {
-        log.debug("DataAccessException: {}" ,ex.getMessage());
+        log.error("DataAccessException: {}" ,ex.getMessage());
         String errorMessage = "Database error occurred: " + ex.getMessage();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(errorMessage));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception ex) {
-        log.debug("Exception, class: {}, message: {}" ,ex.getClass().getSimpleName(), ex.getMessage());
+        log.error("Exception, class: {}, message: {}" ,ex.getClass().getSimpleName(), ex.getMessage());
         if(ex instanceof ClientRequestException clientRequestException){
             return ResponseEntity.status(clientRequestException.getStatusCode()).body(ApiResponse.error(clientRequestException.getMessage(), clientRequestException.getData()));
         }
