72f994529f92c65b4df7808d8c62607830341a4b
Yogen <yogendrapokhrel777@gmail.com>
2024-12-28 00:23:30 -0600

    Folder renamed as external-services

--- keycloak/resource-generator/dist/index.js
+++
@@ -1,4 +0,0 @@
-import seeder from "./process.js";
-console.log("Seeding Started");
-seeder.main().then((data) => { console.log("Seeding completed"); })
-    .catch((error) => { console.error("Seeding failed:" + error.message); });
